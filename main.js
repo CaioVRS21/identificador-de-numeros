a2f2dad7edb5950bca6280d62a2b5f104683c70d
CaioVRS21 <veras.caio12@gmail.com>
2022-07-02 18:23:27 -0400

    .

---
+++ main.js
@@ -0,0 +1,28 @@
+function Calc(){
+    let numero = document.getElementById('CampoNumtxt')
+    
+    if (numero.value.length === 0){
+        window.alert('[ERRO] Por favor, digite alto e tente novamente');
+        return;
+    }
+
+    if (numero.value.length != 0){
+        numero = Number(numero.value)
+        inteiro = Number.isInteger(numero);
+        notNum = Number.isNaN(numero);
+        raiz = Math.sqrt(numero);
+        redondadoBaixo = Math.floor(numero);
+        redondadoCima = Math.ceil(numero);
+        //duasCasas = Number(numero.toFixed(2));
+        container2.innerHTML = `
+        <p>O valor digitado foi ${numero}</p>
+        <p>O número ${numero} é inteiro? ${inteiro}</p>
+        <p>O número ${numero} é um NaN (Not a Number)? ${notNum}</p>
+        <p>Sua raíz quadrada é de: ${raiz}</p>
+        
+        <p>Este valor arredondado para cima é: ${redondadoCima}</p>
+        <p>Este valor arredondado para baixo é: ${redondadoBaixo}</p>
+        `;
+        return;
+    }
+}
