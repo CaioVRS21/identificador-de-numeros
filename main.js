c7cf69c1b296e9c3a9b96a437951d2e976a13999
CaioVRS21 <veras.caio12@gmail.com>
2022-07-03 16:38:41 -0400

    .

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ function Calc(){
         raiz = Math.sqrt(numero);
         redondadoBaixo = Math.floor(numero);
         redondadoCima = Math.ceil(numero);
-        //duasCasas = Number(numero.toFixed(2));
+        duasCasas = numero.toFixed(2);
         container2.innerHTML = `
         <p>O valor digitado foi ${numero}</p>
         <p>O número ${numero} é inteiro? ${inteiro}</p>
         <p>O número ${numero} é um NaN (Not a Number)? ${notNum}</p>
         <p>Sua raíz quadrada é de: ${raiz}</p>
-        
+        <p>Este valor até duas casa decimais fica: ${duasCasas}</p>
         <p>Este valor arredondado para cima é: ${redondadoCima}</p>
         <p>Este valor arredondado para baixo é: ${redondadoBaixo}</p>
         `;
